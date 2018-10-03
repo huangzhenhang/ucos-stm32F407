af4508dbe777ef0038c0518957ba8d5823803ec8
SillyBug <15259171313@sina.cn>
2018-10-03 22:13:23 +0800

    Revert "multiple_task"
    This reverts commit c98514688598e4fed19f2cbef845612bb294fec5.

--- source/OBJ/main.d
+++ source/OBJ/main.d
@@ -38,7 +38,12 @@
 ..\obj\main.o: ..\SYSTEM\delay\delay.h
 ..\obj\main.o: ..\SYSTEM\usart\usart.h
 ..\obj\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+<<<<<<< HEAD
 ..\obj\main.o: ..\HARDWARE\LCD\stm32lcd.h
+=======
+..\obj\main.o: ..\HARDWARE\LED\led.h
+..\obj\main.o: ..\HARDWARE\LCD\lcd.h
+>>>>>>> parent of c985146... multiple_task
 ..\obj\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
 ..\obj\main.o: ..\HARDWARE\SRAM\sram.h
 ..\obj\main.o: ..\PICTURE\piclib.h
